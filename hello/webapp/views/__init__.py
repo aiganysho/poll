3af4dc0fd7ed847f8c3071b1a2a34be7c1250a7e
Aiganysh <aiganyshx@gmail.com>
2021-04-03 16:27:37 +0600

    Создана папка __init__.py

---
+++ hello/webapp/views/__init__.py
@@ -0,0 +1,15 @@
+from webapp.views.poll import (
+    IndexView,
+    PollDetailView,
+    PollCreate,
+    PollUpdate,
+    PollDelete,
+)
+
+from webapp.views.choice import (
+    ChoiceView,
+    ChoiceDetailView,
+    ChoiceCreate,
+    ChoiceUpdate,
+    ChoiceDelete
+)
